c253f812043b365fa0030a54eafbc0fc6eea366a
soufiane <jabersoufiane605@gmail.com>
2024-03-02 17:50:27 +0100

    repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="bg-white text-xl border-b-[2px] border-stone-100  text-black h-[8vh] flex items-center justify-between px-5 fixed w-screen">
             <p>🕌</p>
             <p className="text-center font-medium py-4">Al Quran</p>
-            <a href="https://github.com">
+            <a href="https://github.com/Soufi-ane/Quran/">
                 <FaGithub />
             </a>
         </div>
